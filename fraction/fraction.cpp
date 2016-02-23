9a2ddc01c83eb8c01f724efd449b552eccf5a7b1
jzli106 <eyecueall@yahoo.com>
2016-02-23 00:30:01 -0800

    added more to fraction.cpp
    most of the function are done
    need ^, how to return an object by reference without calling new or
    making a local object in the function.

--- fraction/fraction.cpp
+++ fraction/fraction.cpp
@@ -1,9 +1,9 @@
 #include "fraction.h"
 
 // constructor//?????????????????????????????????????????????????
-fraction::fraction(int n, int d)
+fraction::fraction(long n, int d)
 {
-    setNum(n,d);
+    setNum((int)n,d);
 //    int sign = 1;
 //    if(d == 0)
 //        throw ZERO_DENOM;
@@ -36,11 +36,11 @@ fraction::fraction(double x)
         tempX=tempX*10;
         denom *=10;
 
-        std::cout<<"tempx: "<<tempX<<" tempX: int: "<<(int) tempX<<std::endl;
+        //std::cout<<"tempx: "<<tempX<<" tempX: int: "<<(int) tempX<<std::endl;
         //std::cout<<"texP : "<<tempX<<std::endl;
 
-        // infinity loop here
-        if(tempX > 1000000)
+        // stop the infinity loop here
+        if(tempX > 90000000 || tempX < -90000000)
             break;
 
     }
@@ -68,6 +68,7 @@ fraction& fraction::operator=(const fraction &other)
 fraction& fraction::operator=(int other)
 {
     setNum(other, 1);
+    return *this;
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -76,6 +77,17 @@ fraction& fraction::operator=(double other)
     fraction temp(other);
     num = temp.numerator();
     denom = temp.denominator();
+    return *this;
+}
+
+int fraction::get_num()
+{
+    return num;
+}
+
+int fraction::get_denom()
+{
+    return denom;
 }
 
 void fraction::setValue(int n, int d)
@@ -119,6 +131,8 @@ fraction& fraction::operator^=(const fraction &other)
 {
     //not sure yet
 }
+
+
 fraction& fraction::operator+=(int other)
 {
     fraction c(other,1);
@@ -188,7 +202,9 @@ void fraction::setNum(int n, int d)
     int sign = 1;
     if(d == 0)
         throw ZERO_DENOM;
-    denom < 0  ? sign = -1:sign = 1;
+
+    // if the denom is negative make it positive and change num to negative
+    sign = (d < 0  ? -1: 1);
     denom = sign*d;
     num = sign*n;
 
@@ -219,13 +235,19 @@ int fraction::gcd(int p, int q)
 
 
 // need to remove the first const
-fraction& operator+(fraction &x, const fraction &y)
+fraction& operator+(const fraction &x, const fraction &y)
 {
     int n = x.num*y.denom+x.denom*y.num;
     int d = x.denom*y.denom;
-    fraction a (n,d);
-    return a;
-
+    //fraction a (n,d);
+    //return a;
+    //int n = x.get_num()*y.get_denom()-x.get_denom()*y.get_num();
+    //int d = x.get_denom()*y.get_denom();
+    //int ja = x.num;
+    //int n = x.num*y.denom+x.denom*y.num;
+    //int d = x.denom*y.denom;
+    fraction *a = new fraction(n,d);
+    return *a;
 }
 
 fraction& operator-(const fraction &x, const fraction &y)
@@ -258,8 +280,8 @@ fraction& operator^(const fraction &x, const fraction &y){}
 
 fraction& operator+(const fraction &x, int y)
 {
-
-
+    fraction fraX(y);
+    return (x+fraX);
 }
 
 fraction& operator-(const fraction &x, int y)
@@ -273,105 +295,291 @@ fraction& operator-(const fraction &x, int y)
     return (x - c);
 }
 
-fraction& operator*(const fraction &x, int y){}
+fraction& operator*(const fraction &x, int y)
+{
+    fraction fraX(y);
+    return (x*fraX);
+}
 
-fraction& operator/(const fraction &x, int y){}
+fraction& operator/(const fraction &x, int y)
+{
+    fraction fraX(y);
+    return (x/fraX);
+}
 
-fraction& operator^(const fraction &x, int y){}
+fraction& operator^(const fraction &x, int y)
+{
 
-fraction& operator+(int x, const fraction &y){}
+}
 
-fraction& operator-(int x, const fraction &y){}
+fraction& operator+(int x, const fraction &y)
+{
+    fraction fraX(y);
+    return (x+fraX);
+}
 
-fraction& operator*(int x, const fraction &y){}
+fraction& operator-(int x, const fraction &y)
+{
+    fraction fraX(y);
+    return (x-fraX);
+}
 
-fraction& operator/(int x, const fraction &y){}
+fraction& operator*(int x, const fraction &y)
+{
+    fraction fraX(y);
+    return (x*fraX);
+}
 
+fraction& operator/(int x, const fraction &y)
+{
+    fraction fraX(y);
+    return (x/fraX);
+}
+
+
+////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 fraction& operator^(int x, const fraction &y){}
 
 
-fraction& operator+(const fraction &x, double y){}
+fraction& operator+(const fraction &x, double y)
+{
+    fraction fraX(y);
+    return (x+fraX);
+}
 
-fraction& operator-(const fraction &x, double y){}
+fraction& operator-(const fraction &x, double y)
+{
+    fraction fraX(y);
+    return (x-fraX);
+}
 
-fraction& operator*(const fraction &x, double y){}
+fraction& operator*(const fraction &x, double y)
+{
+    fraction fraX(y);
+    return (x*fraX);
+}
 
-fraction& operator/(const fraction &x, double y){}
+fraction& operator/(const fraction &x, double y)
+{
+    fraction fraX(y);
+    return (x/fraX);
+}
 
+////++++++++++++++++++++++++++++++++++++++++++++++++++++++
 fraction& operator^(const fraction &x, double y){}
 
-fraction& operator+(double x, const fraction &y){}
+fraction& operator+(double x, const fraction &y)
+{
+    fraction fraX(x);
+    return (fraX+y);
+}
 
-fraction& operator-(double x, const fraction &y){}
+fraction& operator-(double x, const fraction &y)
+{
+    fraction fraX(x);
+    return (fraX-y);
+}
 
-fraction& operator*(double x, const fraction &y){}
+fraction& operator*(double x, const fraction &y)
+{
+    fraction fraX(x);
+    return (fraX*y);
+}
 
-fraction& operator/(double x, const fraction &y){}
+fraction& operator/(double x, const fraction &y)
+{
+    fraction fraX(x);
+    return (fraX/y);
+}
 
+
+///++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 fraction& operator^(double x, const fraction &y){}
 
 
-bool operator==(const fraction &x, const fraction &y){}
+bool operator==(const fraction &x, const fraction &y)
+{
+    // x.num*y.denom == x.denom*y.num;
+    return (x.num*y.denom == x.denom*y.num);
+}
 
-bool operator!=(const fraction &x, const fraction &y){}
+bool operator!=(const fraction &x, const fraction &y)
+{
+    return (x.num*y.denom != x.denom*y.num);
+}
 
-bool operator<=(const fraction &x, const fraction &y){}
+bool operator<=(const fraction &x, const fraction &y)
+{
+    return (x.num*y.denom <= x.denom*y.num);
+}
 
-bool operator>=(const fraction &x, const fraction &y){}
+bool operator>=(const fraction &x, const fraction &y)
+{
+    return (x.num*y.denom >= x.denom*y.num);
+}
 
-bool operator<(const fraction &x, const fraction &y){}
+bool operator<(const fraction &x, const fraction &y)
+{
+    return (x.num*y.denom < x.denom*y.num);
+}
 
-bool operator>(const fraction &x, const fraction &y){}
-
-
-bool operator==(const fraction &x, int y){}
-
-bool operator!=(const fraction &x, int y){}
-
-bool operator<=(const fraction &x, int y){}
-
-bool operator>=(const fraction &x, int y){}
-
-bool operator<(const fraction &x, int y){}
-
-bool operator>(const fraction &x, int y){}
-
-bool operator==(int x, const fraction &y){}
-
-bool operator!=(int x, const fraction &y){}
-
-bool operator<=(int x, const fraction &y){}
-
-bool operator>=(int x, const fraction &y){}
-
-bool operator<(int x, const fraction &y){}
-
-bool operator>(int x, const fraction &y){}
+bool operator>(const fraction &x, const fraction &y)
+{
+    return (x.num*y.denom > x.denom*y.num);
+}
 
 
-bool operator==(const fraction &x, double y){}
+bool operator==(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x == tempFra);
+}
 
-bool operator!=(const fraction &x, double y){}
+bool operator!=(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x != tempFra);
+}
 
-bool operator<=(const fraction &x, double y){}
+bool operator<=(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x <= tempFra);
+}
 
-bool operator>=(const fraction &x, double y){}
+bool operator>=(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x >= tempFra);
+}
 
-bool operator<(const fraction &x, double y){}
+bool operator<(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x < tempFra);
+}
 
-bool operator>(const fraction &x, double y){}
+bool operator>(const fraction &x, int y)
+{
+    fraction tempFra(y);
+    return(x > tempFra);
+}
 
-bool operator==(double x, const fraction &y){}
+bool operator==(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra == y);
+}
 
-bool operator!=(double x, const fraction &y){}
+bool operator!=(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra != y);
+}
 
-bool operator<=(double x, const fraction &y){}
+bool operator<=(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra <= y);
+}
 
-bool operator>=(double x, const fraction &y){}
+bool operator>=(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra >= y);
+}
 
-bool operator<(double x, const fraction &y){}
+bool operator<(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra < y);
+}
 
-bool operator>(double x, const fraction &y){}
+bool operator>(int x, const fraction &y)
+{
+    fraction tempFra(x);
+    return(tempFra > y);
+}
+
+
+bool operator==(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x==tempFra);
+}
+
+
+bool operator!=(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x!=tempFra);
+}
+
+
+bool operator<=(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x<=tempFra);
+}
+
+
+bool operator>=(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x>=y);
+}
+
+
+bool operator<(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x<y);
+}
+
+
+bool operator>(const fraction &x, double y)
+{
+    fraction tempFra(y);
+    return (x>tempFra);
+}
+
+bool operator==(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (x==tempFra);
+}
+
+
+bool operator!=(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (tempFra!=y);
+}
+
+
+bool operator<=(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (tempFra>y);
+}
+
+bool operator>=(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (tempFra>=y);
+}
+
+bool operator<(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (tempFra<y);
+}
+
+bool operator>(double x, const fraction &y)
+{
+    fraction tempFra(x);
+    return (tempFra>y);
+}
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ostream& operator<<(ostream& out, const fraction &frac)
@@ -381,7 +589,7 @@ ostream& operator<<(ostream& out, const fraction &frac)
     {
         cout << frac.num << "/" << frac.denom;
     }
-
+    return out;
 }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -389,6 +597,8 @@ istream& operator>>(istream& in, fraction &frac)
 {
     if (&in == &cin)
     {
+
         in >> frac.num >> frac.denom;
     }
+    return in;
 }
--- fraction/main.cpp
+++ fraction/main.cpp
@@ -3,21 +3,129 @@
 #include "mixed.h"
 
 using namespace std;
+void test_constructor();
+void test_boolean_double();
+void addition();
 
 int main()
 {
-//    mixed a,b,c;
-//    c = a + b;
 
+    /// comment in the test u want u run
+    fraction half(11,2);
+    //test_constructor();
 
-    fraction f(1,2);
-    fraction c(1,2);
-    fraction a, b(22,2), d(1,4),z;
-    cout<<"before"<<endl;
-    z = b - 2;
-
-    cout<<"z: "<<z<<endl;
-    cout<<"z: "<<(f - c)<<endl;
+    //test_boolean_double();
+    addition();
     return 0;
 }
 
+void test_constructor()
+{
+    int     one = 1;
+    int     two = 2;
+
+    //    int& numerator();
+    //    int& denominator();
+    cout<<"Created fraction 1/2 using numerator = "<<one<<", denominator = "<<two<<". "<<endl;
+    fraction half(one,two);
+    cout<<"Fraction output: "<<half<<endl<<endl;
+
+    cout<<"Created fraction 1/2 using numerator = 1, denominator = -2.  "<<endl;
+    fraction negative_half(1,-2);
+    cout<<"Fraction output: "<<negative_half<<endl<<endl;
+
+    cout<<"Created fraction 1/2 using numerator = -1, denominator = -2. "<<endl;
+    fraction __half(-1,-2);
+    cout<<"Fraction output: "<<__half<<endl<<endl;
+
+    cout<<"Created fraction 1/2 using numerator = -1, denominator = 2. "<<endl;
+    fraction __1half(-1,2);
+    cout<<"Fraction output: "<<__1half<<endl<<endl;
+
+    cout<<"Created fraction using integer 2.0. "<<endl;
+    fraction double_F(2.0);
+    cout<<"Fraction output: "<<double_F<<endl<<endl;
+
+    cout<<"Created fraction using 2.22222. "<<endl;
+    fraction double_A(2.22222);
+    cout<<"Fraction output: "<<double_A<<endl<<endl;
+
+    cout<<"Created fraction using -2.333. "<<endl;
+    fraction double_nega(-2.333);
+    cout<<"Fraction output: "<<double_nega<<endl<<endl;
+
+    cout<<"Created fraction using double 4.444. "<<endl;
+    fraction double_pos(4.444);
+    cout<<"Fraction output: "<<double_pos<<endl<<endl;
+
+    //444 is consider type long
+    // had trouble with int and double cuz the ambiguous error
+    cout<<"Created fraction using double 444. "<<endl;
+    fraction int_p(444);
+    cout<<"Fraction output: "<<int_p<<endl<<endl;
+
+    cout<<"Created fraction using double -88888. "<<endl;
+    fraction int_p2(-88888);
+    cout<<"Fraction output: "<<int_p2<<endl<<endl;
+
+    cout<<"Created fraction using double 0. "<<endl;
+    fraction int_p3(0);
+    cout<<"Fraction output: "<<int_p3<<endl<<endl;
+}
+
+void test_boolean_double()
+{
+    fraction two_two(2.2);
+    fraction half(1,2);
+    fraction n_half(-1,2);
+    fraction _11_5(11,5);
+    fraction _3(3);
+    fraction _3_33(3.3);
+
+    cout<<"==Testing for double with greater than sign \">\""<<endl;
+    cout<<"2.2 > 1/2: "<<(2.2 > half ? "true.":"false.")<<endl;
+    cout<<"2.2 > -1/2: "<<(2.2 > n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 > -1/2: "<<(-2.2 > n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 > 1/2: "<<(-2.2 > half ? "true.":"false.")<<endl;
+    cout<<"0.5 > 1/2: "<<(0.5 > half ? "true.":"false.")<<endl;
+
+    cout<<endl<<endl;
+    cout<<"==Testing for double with greater than or equal sign \">=\""<<endl;
+    cout<<"2.2 >= 1/2: "<<(2.2 >= half ? "true.":"false.")<<endl;
+    cout<<"2.2 >= -1/2: "<<(2.2 >= n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 >= -1/2: "<<(-2.2 >= n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 >= 1/2: "<<(-2.2 >= half ? "true.":"false.")<<endl;
+    cout<<"0.5 >= 1/2: "<<(0.5 >= half ? "true.":"false.")<<endl;
+
+    cout<<endl<<endl;
+    cout<<"==Testing for double with greater than sign \"<\""<<endl;
+    cout<<"2.2<1/2: "<<(2.2 < half ? "true.":"false.")<<endl;
+    cout<<"2.2 < -1/2: "<<(2.2 < n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 < -1/2: "<<(-2.2 < n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 < 1/2: "<<(-2.2 < half ? "true.":"false.")<<endl;
+    cout<<"0.5 < 1/2: "<<(0.5 < half ? "true.":"false.")<<endl;
+
+    cout<<endl<<endl;
+    cout<<"==Testing for double with greater than or equal sign \"<=\""<<endl;
+    cout<<"2.2 <= 1/2: "<<(2.2 <= half ? "true.":"false.")<<endl;
+    cout<<"2.2 <= -1/2: "<<(2.2 <= n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 <= -1/2: "<<(-2.2 <= n_half ? "true.":"false.")<<endl;
+    cout<<"-2.2 <= 1/2: "<<(-2.2 <= half ? "true.":"false.")<<endl;
+    cout<<"0.5 <= 1/2: "<<(0.5 >= half ? "true.":"false.")<<endl;
+
+    cout<<"2.2 == 1/2: "<<(2.2 == half ? "true.":"false.")<<endl;
+
+
+
+}
+
+void addition()
+{
+    fraction half(1,2);
+    fraction one_third(1,3);
+    fraction one(1);
+
+    cout<<"half + 1/3: "<<(half+one_third)<<endl;
+    cout<<".5+1/2: "<<(.5+half)<<endl;
+
+}
